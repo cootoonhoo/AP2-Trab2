0473068a3c29ec39a1ebee8a046ef285fdee9d2f
Marco Antonio <cootoonhoo@gmail.com>
2023-10-05 12:00:21 -0300

    Fazendo a alocação da matriz e lista

--- mktpp.c
+++ mktpp.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* Structs do programa */
 typedef struct 
@@ -10,34 +11,67 @@ typedef struct
 
 typedef struct
 {
-    char NomeFilia[50];
-    Produto *listaProdutos;
+    char NomeFilial[50];
+    Produto **MatrizProdutosTrimestre;
 } Filial;
 
 /* Variáveis globais no escopo da aplicação*/
 int QuantidadeProdutosCatalogo = 0;
 int QuantidadeFiliais = 0;
+Produto ***CuboProdutos;
+Filial *ListaFiliais;
+
+/* Metodos para o Cubo */
+
+int AlocarCuboProdutos()
+{
+    //Primeira dimensão = Camada de Trimestres do ano
+    CuboProdutos = (Produto ***)malloc(4 * sizeof(Produto**));
+    if(CuboProdutos == NULL) return 0;
+
+    for(int i = 0; i < 4; i++)
+    {
+        // Segunda dimensão = Camada de Filiais
+        *(CuboProdutos+i) = (Produto **)malloc(QuantidadeFiliais * sizeof(Produto*));
+        if(*(CuboProdutos+i) == NULL) return 0;
+
+        for(int j = 0; j < QuantidadeFiliais; j++)
+        {
+            // Terceira dimensõa = Camda de produtos
+            *(*(CuboProdutos+i)+j) = (Produto *)malloc(QuantidadeProdutosCatalogo * sizeof(Produto));
+            if(*(*(CuboProdutos+i)+j) == NULL) return 0;
+        }
+    }
+    return 1;
+}
+
+int AlocarListaFiliais()
+{
+    ListaFiliais = (Filial *)malloc(QuantidadeFiliais * sizeof(Filial));
+    if(ListaFiliais == NULL) return 0;
+    for(int i = 0; i < QuantidadeFiliais; i++)
+        for(int j = 0; j < 4; j++)
+            ListaFiliais[i].MatrizProdutosTrimestre[j] = (((CuboProdutos)+i)+j);
+    return 1;
+}
+
+
+/* Metodos auxiliares */
+int getStringLength(char text[255])
+{
+    int i;
+    for(i = 0; text[i]!='\0'; i++);
+    return i;
+}
 
 /* Métodos de menu */
-void HeadderBoasVindas()
+void Headder()
 {
     printf("\t*****************************************\n");
     printf("\t*\t\t\t\t\t*\n");
     printf("\t*\tSISTEMA DE GESTAO MARKET++\t*\n");
     printf("\t*\t\t\t\t\t*\n");
     printf("\t*****************************************\n");
-    printf("\n______________________________________________________\n");
-}
-
-void HeadderSistema(char text[])
-{
-    printf("\t*****************************************\n");
-    printf("\t*\t\t\t\t\t*\n");
-    printf("\t*\t\t %s \t\t\t*\n", text);
-    printf("\t*\t\t\t\t\t*\n");
-    printf("\t*****************************************\n");
-    printf("\nProdutos:%d\n", QuantidadeProdutosCatalogo);
-    printf("Filiais:%d", QuantidadeFiliais);
     printf("\n______________________________________________________\n");
 }
 
@@ -57,13 +91,16 @@ int LeituraQuantidadeProdutos()
     else return 0;
 }
 
-void InicializaSistema()
+
+int InicializaSistema()
 {
     int ValidacaoInput = 1;
+    int ValidacaoCubo = 0;
+    int ValidaListaFiliais = 0;
     do
     {
         system("cls");
-        HeadderBoasVindas();
+        Headder();
         if(!ValidacaoInput)
         {
             printf("Valor anterior invalido!\n\n");
@@ -74,20 +111,39 @@ void InicializaSistema()
     do
     {
         system("cls");
-        HeadderBoasVindas();
+        Headder();
         if(!ValidacaoInput)
         {
             printf("Valor anterior invalido!\n\n");
         }
         ValidacaoInput = LeituraQuantidadeFiliais();
     }while(!ValidacaoInput);
+
+    ValidacaoCubo = AlocarCuboProdutos();
+    if(!ValidacaoCubo)
+    {
+        system("cls");
+        system("color 4F");
+        printf("Erro na Alocacao do Cubo de produtos!");
+        return 0;
+    }
+
+    ValidaListaFiliais = AlocarListaFiliais();
+    if(!ValidacaoCubo)
+    {
+        system("cls");
+        system("color 4F");
+        printf("Erro na Alocacao da lista de Filiaias!");
+        return 0;
+    }    
+    return 1;
 }
 
 
 int main()
 {
-    InicializaSistema();
-        system("cls");
-    HeadderSistema("Menu");
+    int ValidadorDeRotinas = 0;
+    ValidadorDeRotinas = InicializaSistema();
+    if(!ValidadorDeRotinas) return 0;
     return 0;
 }
